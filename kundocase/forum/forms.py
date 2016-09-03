55b777453883698182a315bd76f16a2f4dabbb94
HaraldNordgren <haraldnordgren@gmail.com>
2016-09-03 16:33:02 +0200

    Allow users to add answers

--- kundocase/forum/forms.py
+++ kundocase/forum/forms.py
@@ -1,11 +1,23 @@
 from django.forms import ModelForm
 from django.core.exceptions import ValidationError
-from .models import Question
+from .models import Question, Answer
 
 class QuestionForm(ModelForm):
     class Meta:
         model = Question
         fields = ['title', 'content', 'user_name', 'user_email']
+
+    def clean_user_email(self):
+        if self.cleaned_data['user_name'] == self.cleaned_data['user_email']:
+            raise ValidationError("User name and email must be different")
+
+        return self.cleaned_data['user_email']
+
+
+class AnswerForm(ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content', 'user_name', 'user_email']
 
     def clean_user_email(self):
         if self.cleaned_data['user_name'] == self.cleaned_data['user_email']:
--- kundocase/forum/views.py
+++ kundocase/forum/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 
-from kundocase.forum.models import Question
-from kundocase.forum.forms import QuestionForm
+from kundocase.forum.models import Question, Answer
+from kundocase.forum.forms import QuestionForm, AnswerForm
+
 
 def startpage(request):
     questions = Question.objects.all().order_by("created")
@@ -12,7 +13,6 @@ def startpage(request):
 
 
 def add_question(request):
-
     if request.method == 'GET':
         form = QuestionForm()
 
@@ -27,8 +27,20 @@ def add_question(request):
 
 def question(request, id):
     question = get_object_or_404(Question, id=id)
-    answers = question.answer_set.all().order_by("-created")
+    answers = question.answer_set.all().order_by("created")
+
+    if request.method == 'GET':
+        form = AnswerForm()
+    elif request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.question = question
+            answer.save()
+            form = AnswerForm()
+
     return render(request, "forum/question.html", {
         "question": question,
         "answers": answers,
+        "form": form
     })
