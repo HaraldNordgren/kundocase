1da593c3e63e2ad3c07473a6db790e23faffcf2f
HaraldNordgren <haraldnordgren@gmail.com>
2016-09-05 00:07:28 +0200

    Post answer to API too

--- kundocase/api/v1/views.py
+++ kundocase/api/v1/views.py
@@ -2,10 +2,10 @@ import json
 
 from django.shortcuts import get_object_or_404
 from django.core import serializers
-from django.http import JsonResponse, Http404, HttpResponseBadRequest, HttpRequest
+from django.http import JsonResponse, Http404, HttpResponseBadRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from kundocase.forum.models import Question
+from kundocase.forum.models import Question, Answer
 
 
 @csrf_exempt
@@ -31,17 +31,19 @@ def questions(request, question_id=None):
             if field not in data:
                 return HttpResponseBadRequest("Request missing '%s' field" % field)
             if not isinstance(data[field], basestring):
-                return HttpResponseBadRequest("Data needs to be string type")
+                return HttpResponseBadRequest("Values need to be string type")
             new_question[field] = data[field]
 
         Question.objects.create(**new_question)
-        return HttpRequest(status=200)
+        return HttpResponse(status=200)
 
     return Http404
 
 
+@csrf_exempt
 def answers(request, question_id, answer_id=None):
     question = get_object_or_404(Question, id=question_id)
+
     if request.method == 'GET':
         if answer_id:
             answer = get_object_or_404(question.answer_set, id=answer_id)
@@ -52,3 +54,22 @@ def answers(request, question_id, answer_id=None):
             raw_data = serializers.serialize('python', answers)
             actual_data = [answer['fields'] for answer in raw_data]
             return JsonResponse(actual_data, safe=False)
+
+    elif request.method == 'PUT':
+        if answer_id:
+            return Http404
+        data = json.loads(request.body)
+        new_answer = {}
+        for field in Answer._meta.get_all_field_names():
+            if field in ['id', 'created', 'question', 'question_id']:
+                continue
+            if field not in data:
+                return HttpResponseBadRequest("Request missing '%s' field" % field)
+            if not isinstance(data[field], basestring):
+                return HttpResponseBadRequest("Values need to be string type")
+            new_answer[field] = data[field]
+
+        Answer.objects.create(question=question, **new_answer)
+        return HttpResponse(status=200)
+
+    return Http404
