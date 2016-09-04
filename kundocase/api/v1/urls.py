52ff74533ebadeed28afba02f7766567614d5d62
HaraldNordgren <haraldnordgren@gmail.com>
2016-09-04 21:33:06 +0200

    API to get answers

--- kundocase/api/v1/urls.py
+++ kundocase/api/v1/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 
-from .views import questions
+from .views import questions, answers
 
 
 urlpatterns = [
+    url(r"^questions/(?P<question_id>\d+)/answers/$", answers),
+    url(r"^questions/(?P<question_id>\d+)/answers/(?P<answer_id>\d+)/$", answers),
+
     url(r"^questions/$", questions),
     url(r"^questions/(?P<question_id>\d+)/$", questions),
 ]
--- kundocase/api/v1/views.py
+++ kundocase/api/v1/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.shortcuts import get_object_or_404
 from django.core import serializers
 from django.http import JsonResponse
@@ -7,7 +5,6 @@ from kundocase.forum.models import Question
 
 
 def questions(request, question_id=None):
-
     if request.method == 'GET':
         if question_id:
             question = get_object_or_404(Question, id=question_id)
@@ -16,4 +13,18 @@ def questions(request, question_id=None):
         else:
             raw_data = serializers.serialize('python', Question.objects.all().order_by("created"))
             actual_data = [question['fields'] for question in raw_data]
+            return JsonResponse(actual_data, safe=False)
+
+
+def answers(request, question_id, answer_id=None):
+    question = get_object_or_404(Question, id=question_id)
+    if request.method == 'GET':
+        if answer_id:
+            answer = get_object_or_404(question.answer_set, id=answer_id)
+            raw_data = serializers.serialize('python', [answer])[0]
+            return JsonResponse(raw_data['fields'])
+        else:
+            answers = question.answer_set.all().order_by("created")
+            raw_data = serializers.serialize('python', answers)
+            actual_data = [answer['fields'] for answer in raw_data]
             return JsonResponse(actual_data, safe=False)
