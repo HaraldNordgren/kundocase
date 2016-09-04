841fbe1e056edfb282f1ff89ed9978dae49b4025
HaraldNordgren <haraldnordgren@gmail.com>
2016-09-04 22:54:31 +0200

    Post question to API and verfiy input

--- kundocase/api/v1/views.py
+++ kundocase/api/v1/views.py
@@ -1,9 +1,14 @@
+import json
+
 from django.shortcuts import get_object_or_404
 from django.core import serializers
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404, HttpResponseBadRequest, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
+
 from kundocase.forum.models import Question
 
 
+@csrf_exempt
 def questions(request, question_id=None):
     if request.method == 'GET':
         if question_id:
@@ -14,6 +19,25 @@ def questions(request, question_id=None):
             raw_data = serializers.serialize('python', Question.objects.all().order_by("created"))
             actual_data = [question['fields'] for question in raw_data]
             return JsonResponse(actual_data, safe=False)
+
+    elif request.method == 'PUT':
+        if question_id:
+            return Http404
+        data = json.loads(request.body)
+        new_question = {}
+        for field in Question._meta.get_all_field_names():
+            if field in ['id', 'created', 'answer']:
+                continue
+            if field not in data:
+                return HttpResponseBadRequest("Request missing '%s' field" % field)
+            if not isinstance(data[field], basestring):
+                return HttpResponseBadRequest("Data needs to be string type")
+            new_question[field] = data[field]
+
+        Question.objects.create(**new_question)
+        return HttpRequest(status=200)
+
+    return Http404
 
 
 def answers(request, question_id, answer_id=None):
